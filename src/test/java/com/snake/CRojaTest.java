61475ba6cad869cb2eafe307a7a475e12d80ad5c
David Trujillo Medina <d.trujillo@unc.edu.ar>
2024-06-01 22:26:23 -0300

    Comidas

---
+++ src/test/java/com/snake/CRojaTest.java
@@ -0,0 +1,21 @@
+package com.snake;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CRojaTest {
+
+    private CRoja cRoja;
+
+    @BeforeEach
+    void setUp() {
+        cRoja = new CRoja();
+    }
+
+    @Test
+    void getTipo() {
+        assertEquals("Multiplicador", cRoja.getTipo());
+    }
+}
