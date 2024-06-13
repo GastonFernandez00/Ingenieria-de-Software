ace071e34277401a169896216ed2bb303c9bebd9
David Trujillo Medina <d.trujillo@unc.edu.ar>
2024-06-13 19:00:29 -0300

    JFrameSnake enhanced (modularizada)

--- src/main/java/com/snake/JFrameSnake.java
+++ src/main/java/com/snake/JFrameSnake.java
@@ -1,22 +1,18 @@
 package com.snake;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Toolkit;
 
 public class JFrameSnake extends JFrame implements KeyListener {
 
@@ -25,14 +21,15 @@ public class JFrameSnake extends JFrame implements KeyListener {
 	private JLabel lblNewLabel;
 	public static JLabel lblPuntaje;
 	private Menu menu;
+	private int bordeLat;
+	private int bordeSup;
+	private PanelFondo fondo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
 					JFrameSnake frame = new JFrameSnake();
-
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,55 +42,20 @@ public class JFrameSnake extends JFrame implements KeyListener {
 	 */
 	public JFrameSnake() {
 		Modos modo;
-		Menu menu = new Menu();
+		menu = new Menu();
 		int opcion = menu.getOpcion(); //guardo la opcion que me da en una variable
 
-		setIconImage(Toolkit.getDefaultToolkit().getImage(JFrameSnake.class.getResource("/img/snake.png")));
-		setTitle("Snake - Game");
-		addKeyListener(this);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 575, 645);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 
-		
-		/** Panel Principal ----------------------------------------------------- */
-		JPanel panelPrincipal = new JPanel();
-		panelPrincipal.setBackground(new Color(100, 130, 2));
-		panelPrincipal.setBounds(0, 0, 559, 606);
-		contentPane.add(panelPrincipal);
-		panelPrincipal.setLayout(null);
-	
-		
-		/** Panel Snake */
-		snake = new PanelSnake(506,23);
-		panelPrincipal.add(snake);
-		
-		int bordeSup = ( panelPrincipal.getHeight() - snake.tam ) / 2;
-		int bordeLat = ( panelPrincipal.getWidth() - snake.tam ) / 2;
+		setupFrame();
+		setupContentPane();
 
-		snake.setBounds(bordeLat,bordeSup,505,505);
-		snake.setLayout(null);
-		snake.setOpaque(false);
-		
-		/** Panel Fondo */
-		PanelFondo fondo = new PanelFondo(506,23); // 506 pixeles de lado y 23 cuadrados de lado
-		panelPrincipal.add(fondo);
-		fondo.setBounds(bordeLat,bordeSup,505,505);
-		fondo.setLayout(null);
-		fondo.setOpaque(false);
-		
-		lblNewLabel = new JLabel("Puntaje :");
-		lblNewLabel.setFont(new Font("Consolas", Font.BOLD, 22));
-		lblNewLabel.setBounds(337, 15, 119, 20);
-		panelPrincipal.add(lblNewLabel);
-		
-		lblPuntaje = new JLabel( "0" ); // <--------------- CONTADOR
-		lblPuntaje.setFont(new Font("Consolas", Font.BOLD, 22));
-		lblPuntaje.setBounds(458, 15, 40, 20);
-		panelPrincipal.add(lblPuntaje);
+		JPanel panelPrincipal = setupPanelPrincipal();
+		setupSnakePanel(panelPrincipal);
+		bordeSup = ( panelPrincipal.getHeight() - snake.tam ) / 2;
+		bordeLat = ( panelPrincipal.getWidth() - snake.tam ) / 2;
+
+		setupFondoPanel(panelPrincipal);
+		setupPuntajeLabel(panelPrincipal);
 
 		//pregunto cual es la opcion y pongo el modo de juego seleccionado
 
@@ -117,6 +79,59 @@ public class JFrameSnake extends JFrame implements KeyListener {
 				break;
 			}
 		
+	}
+
+
+	private void setupFrame() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(JFrameSnake.class.getResource("/img/snake.png")));
+		setTitle("Snake - Game");
+		addKeyListener(this);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 575, 645);
+	}
+
+	private void setupContentPane() {
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+	private JPanel setupPanelPrincipal() {
+		JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setBackground(new Color(100, 130, 2));
+		panelPrincipal.setBounds(0, 0, 559, 606);
+		contentPane.add(panelPrincipal);
+		panelPrincipal.setLayout(null);
+		return panelPrincipal;
+	}
+
+	private void setupSnakePanel(JPanel panelPrincipal) {
+		snake = new PanelSnake(506,23);
+		panelPrincipal.add(snake);
+		snake.setBounds(26,50,505,505);
+		snake.setLayout(null);
+		snake.setOpaque(false);
+	}
+
+	private void setupFondoPanel(JPanel panelPrincipal) {
+		fondo = new PanelFondo(506,23); // 506 pixeles de lado y 23 cuadrados de lado
+		panelPrincipal.add(fondo);
+		fondo.setBounds(bordeLat,bordeSup,505,505);
+		fondo.setLayout(null);
+		fondo.setOpaque(false);
+	}
+
+	private void setupPuntajeLabel(JPanel panelPrincipal) {
+		lblNewLabel = new JLabel("Puntaje :");
+		lblNewLabel.setFont(new Font("Consolas", Font.BOLD, 22));
+		lblNewLabel.setBounds(337, 15, 119, 20);
+		panelPrincipal.add(lblNewLabel);
+
+		lblPuntaje = new JLabel( "0" ); // <--------------- CONTADOR
+		lblPuntaje.setFont(new Font("Consolas", Font.BOLD, 22));
+		lblPuntaje.setBounds(458, 15, 40, 20);
+		panelPrincipal.add(lblPuntaje);
 	}
 
 	/** Boton para avanzar **/
